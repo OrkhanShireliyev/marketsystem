5991bc3217fab7ac2d0e0b295f762e15fbee125c
Orkhan <orkhan.shireliyev@mail.ru>
2023-09-12 00:07:05 +0400

    yeniləndi

---
+++ MarketSystemPro/src/main/java/com/example/marketsystempro/config/UserPrincipalAuthenticationToken.java
@@ -0,0 +1,22 @@
+package com.example.marketsystempro.config;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+public class UserPrincipalAuthenticationToken extends AbstractAuthenticationToken {
+    private final UserPrinciple principle;
+    public UserPrincipalAuthenticationToken(UserPrinciple principle) {
+        super(principle.getAuthorities());
+        this.principle=principle;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public UserPrinciple getPrincipal() {
+        return principle;
+    }
+}
