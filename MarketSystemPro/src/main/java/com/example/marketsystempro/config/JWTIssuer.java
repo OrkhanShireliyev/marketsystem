0246dbef7a4663ad55c65cf8967554ce0ac1b983
Orkhan <orkhan.shireliyev@mail.ru>
2023-09-12 00:06:29 +0400

    yeniləndi

---
+++ MarketSystemPro/src/main/java/com/example/marketsystempro/config/JWTIssuer.java
@@ -0,0 +1,25 @@
+package com.example.marketsystempro.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JWTIssuer {
+    private final JWTProperties jwtProperties;
+    public String issue(Long userId, String email, List<String>roles){
+        return JWT.create()
+                .withSubject(String.valueOf(userId))
+                .withExpiresAt(Instant.now().plus(Duration.of(1, ChronoUnit.DAYS)))
+                .withClaim("e",email)
+                .withClaim("a",roles)
+                .sign(Algorithm.HMAC256(jwtProperties.getSecretKey()));
+    }
+}
